d7f78170109ba33e41a4f36187bdbb3e005567ef
igargaditya <igargaditya@gmail.com>
2025-07-17 01:40:07 +0530

    Uliltiy Methods

--- src/Utility.java
+++ src/Utility.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+public class Utility {
 
-public class utility {
+   public static boolean  validNumberBetween1_3(String input){
+      try {
+         int num = Integer.parseInt(input);
+         return num >= 1 && num <= 3;
+      } catch (NumberFormatException e) {
+         return false; // Not a valid integer
+      }
+   }
 }
