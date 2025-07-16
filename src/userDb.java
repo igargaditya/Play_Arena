b48938d3974676b90faf08fda803c22eff05f5ea
igargaditya <igargaditya@gmail.com>
2025-07-17 01:49:08 +0530

    userDetail DB Checks

--- src/userDb.java
+++ src/userDb.java
@@ -4,17 +4,47 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-public class DbValidation {
-    public static boolean checkUser(Connection connection, Scanner scanner,String email,String password){
+public class userDb {
+    public static boolean checkUser(Connection connection, Scanner scanner, String email, String password) {
         String query = "select * from userDetail where emailId = ? and password = ? ;";
-        try(PreparedStatement statement = connection.prepareStatement(query)){
-            statement.setString(1,email);
-            statement.setString(2,password);
-            try(ResultSet ans = statement.executeQuery()){
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, email);
+            statement.setString(2, password);
+            try (ResultSet ans = statement.executeQuery()) {
                 return ans.next();
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
+
+    public static void addUser(Connection connection, String email, String password) {
+        String query = "Insert into userDetail(emailId,password) values (?,?);";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, email);
+            statement.setString(2, password);
+            int rows = statement.executeUpdate();
+            if (rows > 0) {
+                System.out.println("User Created ");
+            } else {
+                System.out.println("Failed, Try Again");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean checkEmail(Connection connection, String email) {
+        String query = "select * from userDetail where emailId = ? ;";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, email);
+            try (ResultSet ans = statement.executeQuery()) {
+                return ans.next();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+     }
+
 }
