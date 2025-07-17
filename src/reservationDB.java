0ae16c9a37b740e438ef3a8a5b3c1c44eaa415d3
igargaditya <igargaditya@gmail.com>
2025-07-17 15:53:49 +0530

    Delete Reservation: now;

--- src/reservationDB.java
+++ src/reservationDB.java
@@ -34,12 +34,12 @@ public class reservationDB {
 
             System.out.println();
             System.out.println("+-------------------+------------------+----------------+----------+-----------+---------------------+");
-            System.out.println("|    Email ID       |     Guest        |    Contact     | Turf ID  | Bookings  |  Reservation  Date  |");
+            System.out.println("|  Reservation ID   |     Guest        |    Contact     | Turf ID  | Bookings  |  Reservation  Date  |");
             System.out.println("+-------------------+------------------+----------------+----------+-----------+---------------------+");
 
             while (rs.next()) {
                 System.out.printf("| %-17s | %-16s | %-14d | %-8d | %-9d | %-10s |\n",
-                        rs.getString("emailId"),
+                        rs.getInt("RId"),
                         rs.getString("guestName"),
                         rs.getLong("contactNo"),
                         rs.getInt("turfId"),
@@ -50,6 +50,46 @@ public class reservationDB {
             System.out.println("+-------------------+------------------+----------------+----------+-----------+---------------------+\n");
 
         } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean checkReservationId(Connection connection, int reservationId) {
+        String query = "select * from reservations where rId = ? ;";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, reservationId);
+            try (ResultSet ans = statement.executeQuery()) {
+                return ans.next();
+            }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void deleteReservation(Connection connection, int reservationId) {
+        String query = "delete from reservations where rId = ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, reservationId);
+            int affected  = statement.executeUpdate();
+            if(affected>0){
+                System.out.print("Reservation Deleted");
+                try {
+                    for (int i = 0; i < 5; i++) {
+                        System.out.print(".");
+                        Thread.sleep(200);
+                    }
+
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+                System.out.print("\n\n");
+            }
+            else{
+                System.out.println("\nTry Again Later\n");
+            }
+        }
+        catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
