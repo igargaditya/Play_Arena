45653d621758c803c650e56103f9c723e231a2f3
igargaditya <igargaditya@gmail.com>
2025-07-17 13:41:33 +0530

    Utility Methods Added

--- src/Utility.java
+++ src/Utility.java
@@ -1,11 +1,23 @@
 public class Utility {
 
-   public static boolean  validNumberBetween1_3(String input){
+   public static boolean  validNumberBetween(String input,int num1,int num2){
       try {
          int num = Integer.parseInt(input);
-         return num >= 1 && num <= 3;
+         return num >= num1 && num <= num2;
       } catch (NumberFormatException e) {
          return false; // Not a valid integer
+      }
+   }
+   public static boolean isTenDigitNumber(String input) {
+      return input.matches("\\d{10}");
+   }
+
+   public static boolean isInteger(String input) {
+      try {
+         Integer.parseInt(input);
+         return true;
+      } catch (NumberFormatException e) {
+         return false;
       }
    }
 }
